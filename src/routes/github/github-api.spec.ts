366c24db4d3bb7978d4e59c12631eb471a52f960
Hristo <hristo.ivanov@accso.de>
2025-03-13 20:28:04 +0100

    Call expect as async parameter for concurrent testing

--- src/routes/github/github-api.spec.ts
+++ src/routes/github/github-api.spec.ts
@@ -1,9 +1,9 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it } from 'vitest';
 import { GithubApi } from './github-api';
 
 describe('github-api', () => {
     describe('should get a repository', () => {
-        it('should return repository information', async() => {
+        it('should return repository information', async({expect}) => {
             const api = new GithubApi(undefined);
             const response = await api.getRepository('mhevery', 'qwik');
             expect(response).toMatchSnapshot();
